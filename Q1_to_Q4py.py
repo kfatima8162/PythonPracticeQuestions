13d46aa78d42c6872529d585bc3daf0239fec493
kfatima8162 <fatimakashifa04@gmail.com>
2026-02-25 12:11:30 +0530

    file name changed

---
+++ Q1_to_Q4py.py
@@ -0,0 +1,19 @@
+#--Q1--
+print("Twinkle, twinkle, little star,\n\tHow I wonder what you are!\n\t\tUp above the world so high,\n\t\tLike a diamond in the sky.\nTwinkle, twinkle, little star,\n\tHow I wonder what you are")
+
+#--Q2--
+import sys
+print("Python version:")
+print(sys.version)
+
+#--Q3--
+import datetime
+current_datetime = datetime.datetime.now()
+print(current_datetime)
+
+#--Q4--
+from math import pi 
+a=float(input("Enter radius:"))
+b=pi*a**2
+print("Area:",b)
+
